0bcf0bc9330b3d1539cc9bafcf65c1fd58a08f78
fabiomelodev <fabiomelodev@gmail.com>
2026-05-12 00:48:52 -0300

    fix(SiteConfigResource): ajustado layout form

--- app/Filament/Resources/SiteConfigs/Schemas/SiteConfigForm.php
+++ app/Filament/Resources/SiteConfigs/Schemas/SiteConfigForm.php
@@ -2,8 +2,11 @@
 
 namespace App\Filament\Resources\SiteConfigs\Schemas;
 
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class SiteConfigForm
@@ -11,23 +14,43 @@ class SiteConfigForm
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(12)
             ->components([
-                TextInput::make('company_name')
-                    ->required(),
-                TextInput::make('domain'),
-                TextInput::make('brand'),
-                TextInput::make('whatsapp'),
-                TextInput::make('instagram'),
-                TextInput::make('facebook'),
-                Toggle::make('status')
-                    ->required(),
-                Toggle::make('is_finished')
-                    ->required(),
-                TextInput::make('subscription_id')
-                    ->numeric(),
-                TextInput::make('user_id')
-                    ->required()
-                    ->numeric(),
+                Section::make()
+                    ->columnSpan(9)
+                    ->schema([
+                        TextInput::make('company_name')
+                            ->label('Nome da Empresa')
+                            ->required(),
+                        TextInput::make('domain')
+                            ->label('Domínio'),
+                        TextInput::make('whatsapp'),
+                        TextInput::make('instagram'),
+                        TextInput::make('facebook'),
+                    ]),
+                Section::make()
+                    ->columnSpan(3)
+                    ->schema([
+                        FileUpload::make('brand')
+                            ->label('Logo')
+                            ->image(),
+                        Select::make('subscription_id')
+                            ->label('Asaas Assinatura ID')
+                            ->relationship('subscription', 'asaas_subscription_id')
+                            ->disabled(),
+                        Select::make('user_id')
+                            ->label('Cliente')
+                            ->relationship('user', 'name')
+                            ->disabled()
+                            ->required(),
+                        Toggle::make('is_finished')
+                            ->label('Finalizado')
+                            ->inline(false)
+                            ->required(),
+                        Toggle::make('status')
+                            ->inline(false)
+                            ->required(),
+                    ]),
             ]);
     }
 }
