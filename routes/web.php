e81a95416923ad82a936cd5e8cb0a23f6302d8f9
fabiomelodev <fabiomelodev@gmail.com>
2026-05-28 15:15:33 -0300

    feat: nova rota para testar meios de pagamentos pix e boleto

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,31 @@ Route::controller(NewPasswordController::class)->group(function () {
 
     Route::post('/reset-password', 'store')->name('password.update');
 });
+
+if (app()->environment('local') || config('services.asaas.url') !== 'https://www.asaas.com/api/v3') {
+
+    Route::get('/debug/pay/{paymentId}', function (string $paymentId) {
+        $baseUrl = config('services.asaas.url');
+        $token = config('services.asaas.token');
+
+        // Dispara o comando para o Asaas simular que o cliente pagou a cobrança
+        $response = Illuminate\Support\Facades\Http::withHeaders([
+            'access_token' => $token,
+        ])->post("{$baseUrl}/payments/{$paymentId}/simulatePayment");
+
+        if ($response->successful()) {
+            return response()->json([
+                'status' => 'Success',
+                'message' => "The payment {$paymentId} was successfully simulated as PAID.",
+                'asaas_response' => $response->json()
+            ]);
+        }
+
+        return response()->json([
+            'status' => 'Error',
+            'message' => 'Failed to simulate payment.',
+            'asaas_error' => $response->json()
+        ], $response->status());
+    })->name('debug.simulate-payment');
+
+}
