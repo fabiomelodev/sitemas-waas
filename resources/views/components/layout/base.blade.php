da051afba165379860c625d31855593e79a64f8d
fabiomelodev <fabiomelodev@gmail.com>
2026-06-05 09:36:49 -0300

    feat: visualizar a versao do asaas se esta como sandbox

--- resources/views/components/layout/base.blade.php
+++ resources/views/components/layout/base.blade.php
@@ -14,6 +14,14 @@
 
 <body class="bg-white text-dark-900 antialiased">
 
+    @if (config('services.asaas.env') === 'sandbox')
+        <div class="w-full bottom-0 left-0 fixed bg-orange-500 p-1 z-[9999]">
+            <p class="text-xs font-bold text-center text-white">
+                Sandbox
+            </p>
+        </div>
+    @endif
+
     <x-layout.header />
 
     <main>
